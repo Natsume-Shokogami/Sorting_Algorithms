a936f8d7a9e61c4fe652b308a326bbf111ed1d1a
K.Minh <106172204+Natsume-Shokogami@users.noreply.github.com>
2022-11-01 20:31:41 +0700

    Update Sorting.h

--- Sorting.h
+++ Sorting.h
@@ -8,40 +8,39 @@
 
 using namespace std;
 
-template <class type>
-void Swap(type a, type b);
+void Swap(int &a, int &b);
 
 void CopyArray(int* &source, int* &des, int n);
 
-void Heapify(int* &a, int n, int i, int &count_cmp);
+void Heapify(int* &a, int n, int i, long long &count_cmp);
 
-void HeapSort(int* &a, int n, int &count_cmp);
+void HeapSort(int* &a, int n, long long &count_cmp);
 
-void Merge(int* &a, int* &tempArray, int left, int mid, int right, int &count_cmp);
+void Merge(int* &a, int* &tempArray, int left, int mid, int right, long long &count_cmp);
 
 int Min(int a, int b);
 
-void MergeSort(int* &a, int left, int right, int &count_cmp);
+void MergeSort(int* &a, int left, int right, long long &count_cmp);
 
-void QuickSort(int* &a, int left, int right, int &count_cmp);
+
+void QuickSort(int* &a, int left, int right, long long &count_cmp);
 
 int GetMax(int* a, int n);
 
-void RadixSort(int* &a, int n, int &count_cmp);
+void RadixSort(int* &a, int n, long long &count_cmp);
 
-void InsertionSort(int* &a, int n, int &count_cmp);
+void InsertionSort(int* &a, int low, int high, long long &count_cmp);
 
-void FlashSort(int* &a, int n, int &count_cmp);
+void FlashSort(int* &a, int n, long long &count_cmp);
 
-void SelectionSort(int* &a, int n, int &count_cmp);
+void SelectionSort(int* &a, int n, long long &count_cmp);
 
-void BubbleSort(int* &a, int n, int &count_cmp);
+void BubbleSort(int* &a, int n, long long &count_cmp);
 
-void ShellSort(int* &a, int n, int &count_cmp);
+void ShellSort(int* &a, int n, long long &count_cmp);
 
-void ShakerSort(int* &a, int n, int &count_cmp);
+void ShakerSort(int* &a, int n, long long &count_cmp);
 
-void CountingSort(int* &a, int n, int &count_cmp);
-
+void CountingSort(int* &a, int n, long long &count_cmp);
 
 
